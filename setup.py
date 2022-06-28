95be6ae959932b9cdece770c861118f184d42f78
Flavio Piccoli <flavio.piccoli@unimib.it>
2022-06-28 10:55:04 +0200

    Added long description.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
+
 
 setuptools.setup(
 	name='supermail',
-	version='1.1',
+	version='1.2',
 	author="Flavio Piccoli",
 	author_email="dros1986@gmail.com",
 	description="An email client for reading and sending emails.",
+	long_description=long_description,
+    long_description_content_type='text/markdown',
 	# long_description="An email client for reading and sending emails.",
 	# long_description_content_type="text/markdown",
 	url="https://github.com/dros1986/EmailClient",
